2c702a1251ca4139459342a4d156ac7ba7b377d3
Paoleski <lucas.paoleschi@gmail.com>
2020-10-22 19:26:06 -0300

    phonebook update

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import Filter from './components/Filter';
 import PersonInput from './components/PersonInput';
 import Persons from './components/Persons';
 import PersonService from './services/Person';
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,6 +12,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
   const [showAll, setShowAll] = useState(true);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     PersonService.getAll().then((people) => {
@@ -30,20 +32,23 @@ const App = () => {
     const changedObject = { ...nameExists, number: newNumber };
     nameExists
       ? window.confirm('Name already exists, do you want to update the number?')
+      // update HTTP function
         ? PersonService.update(
             changedObject,
             changedObject.id
           ).then((updatedPerson) =>
-            setPersons(
-              persons.map((person) =>
-                person.id !== changedObject.id ? person : updatedPerson
-              )
-            )
-          )
+              setPersons(persons.map((person) => person.id !== changedObject.id ? person : updatedPerson)),
+              handleMessage({text:`${personObject.name} was updated`, type:'success'})
+            ).catch(error => {
+              handleMessage({text:`${personObject.name} was already deleted from the server`, type:'fail'})
+            })
         : alert('keeping it')
+        // create HTTP function
       : PersonService.create(personObject).then((returnedNote) => {
           setPersons(persons.concat(returnedNote));
+          handleMessage({text:`${personObject.name} was added`, type: 'success'})
         });
+
     setNewName('');
     setNewNumber('');
   };
@@ -73,6 +78,14 @@ const App = () => {
     setPersons(persons.filter((person) => person.id !== id));
   };
 
+  const handleMessage = (message) => {
+    console.log(message);
+    setMessage(message);
+    setTimeout(() => {
+      setMessage(null);
+    },3000)
+  }
+
   const whatToShow = showAll
     ? persons
     : persons.filter((person) =>
@@ -80,8 +93,9 @@ const App = () => {
       );
 
   return (
-    <div>
+    <div className='container'>
       <h2>Phonebook</h2>
+      <Notification message={message}></Notification>
       <Filter filteredText={newFilter} handleFilter={handleFiltering}></Filter>
       <h2>Add a new</h2>
       <PersonInput
